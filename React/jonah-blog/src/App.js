5b79dd64690c8e230a43a69fe90467826129e0dc
jonahmichael <jonah.paulin2596@gmail.com>
2025-12-11 01:11:10 +0530

    #4 - Dynamic Values in Templates

--- React/jonah-blog/src/App.js
+++ React/jonah-blog/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 
 function App() {
+   const title = "Jonah's Blog";
+   const likes= 50;
+   const person = { name: 'Jonah', age: 19 };
   return (
     <div className="App">
       <div>
-        <h1>Welcome to Jonah's Blog</h1>
+        <h1>Welcome to {title}</h1>
+        <p>This blog has {likes} likes</p>
+        {/* <p>{person.name} is {person.age} years old</p> */}
+        <a href ={`https://www.google.com/search?q=${person.name}+age`}>Search {person.name}'s age</a>
       </div>
     </div>
   );
