6709c47fb0ee71a3983f041e0373002d30e3616a
jonahmichael <jonah.paulin2596@gmail.com>
2025-12-12 00:42:53 +0530

    #6 - Adding Styles

--- React/jonah-blog/src/Home.js
+++ React/jonah-blog/src/Home.js
@@ -1,8 +1,13 @@
 const Home = () => {
+  const hanfleClick = () => {
+    console.log('Hello, Jonah');
+  }
+
   return ( 
     <div className="home"> 
     <h2>Home Page</h2>
     <p>Welcome to Jonah's Blog. This is the home page where you can find the latest posts and updates.</p> 
+    <button onClick={hanfleClick}>Read More</button>
     </div>
 
    );
